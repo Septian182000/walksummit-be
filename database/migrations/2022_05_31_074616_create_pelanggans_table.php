e2df0e486e9dbfacab2c119c87a51dacd19dad69
MHafizAF <hafiz170301@gmail.com>
2022-05-31 15:21:51 +0700

    feature(migration) add jalur and pelanggan table with seeder and factory

---
+++ database/migrations/2022_05_31_074616_create_pelanggans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pelanggans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_grup');
+            $table->string('nik', 16);
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('no_telp', 16);
+            $table->string('no_telp_orgtua', 16);
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelanggans');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call(PelangganTableSeeder::class);
+        $this->call(JalurTableSeeder::class);
     }
 }
