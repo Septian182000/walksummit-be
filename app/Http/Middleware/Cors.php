4e2e9255e6f126bde6559bfb6768411cdd31b0e7
Ken <musyaffa560@gmail.com>
2022-06-29 16:29:18 +0700

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,7 @@ class Cors
         header('Access-Control-Allow-Origin:  *');
         header('Access-Control-Allow-Origin:  http://walksummit-be.herokuapp.com/');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-        header('Access-Control-Allow-Methods:  POST, PUT');
+        header('Access-Control-Allow-Methods:  GET ,POST, PUT, DELETE');
         return $next($request);
     }
 }
